47f3a2b265122aa7087aecc7b39b3d21c5f2981a
fatih-iver-2106400264 <fatih.iver@boun.edu.tr>
2018-08-27 20:54:08 +0300

    Get Instance Id Token

---
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkFirebaseInstanceIdService.java
@@ -0,0 +1,17 @@
+
+package android.example.com.squawker.fcm;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class SquawkFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(SquawkFirebaseInstanceIdService.class.getSimpleName(), "Refreshed token: " + refreshedToken);
+    }
+
+}
